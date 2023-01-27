f5a7d7820964d64ae7d6b6d77e426f356f725a8d
Jaideep Bhatia <jaideep.bhatia73@gmail.com>
2023-01-27 23:11:11 +0530

    committing WelcomeWorld java file

---
+++ src/WelcomeWorld.java
@@ -0,0 +1,11 @@
+
+public class WelcomeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome Jaideep: Today is " + new java.util.Date());
+		
+	}
+
+}
